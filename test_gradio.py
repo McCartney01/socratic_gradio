088a649d396e1698a7a3f800a718d4c0151308a7
Zhaofeng Li <lzfxxx@gmail.com>
2024-10-28 16:34:56 +0800

    fix: add root_path for nginx proxy

--- test_gradio.py
+++ test_gradio.py
@@ -149,4 +149,4 @@ with gr.Blocks() as iface:
 # 运行Gradio应用
 if __name__ == "__main__":
     iface.queue(default_concurrency_limit=None)
-    iface.launch(server_name="0.0.0.0", max_threads=100)
+    iface.launch(server_name="0.0.0.0", root_path="/socratic", max_threads=100)
